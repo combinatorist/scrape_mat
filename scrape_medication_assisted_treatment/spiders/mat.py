8767276567e6a57bae33db37ffa3dc5d2ba347fc
Tim Eccleston <teccleston@axialhealthcare.com>
2018-02-04 19:50:17 -0600

    promote parsing functions to class-level and reorder

--- scrape_medication_assisted_treatment/spiders/mat.py
+++ scrape_medication_assisted_treatment/spiders/mat.py
@@ -9,36 +9,36 @@ class MatSpider(scrapy.Spider):
     ]
 
     def parse(self, response):
-        def parse_fields(row):
-            return [
-                {
-                    "raw": field.extract(),
-                    "text": field.css("::text").extract(),
-                    "clean": field.css("::text").extract_first().strip(),
-                }
-                for field in row.css("td")
-            ]
-
-        def parse_rows(table):
-            return [
-                {
-                    "raw": row.extract(),
-                    "fields": parse_fields(row),
-                }
-                for row in table.css("tr")
-            ]
-
-        def parse_tables(response):
-            return [
-                {
-                    "raw": table.extract(),
-                    "rows": parse_rows(table),
-                }
-                for table in response.css("table.tablesaw")
-            ]
-
         yield {
             "url": response.url,
             "body": response.body.decode(),
-            "tables": parse_tables(response)
+            "tables": self.parse_tables(response)
         }
+
+    def parse_tables(self, response):
+        return [
+            {
+                "raw": table.extract(),
+                "rows": self.parse_rows(table),
+            }
+            for table in response.css("table.tablesaw")
+        ]
+
+    def parse_rows(self, table):
+        return [
+            {
+                "raw": row.extract(),
+                "fields": self.parse_fields(row),
+            }
+            for row in table.css("tr")
+        ]
+
+    def parse_fields(self, row):
+        return [
+            {
+                "raw": field.extract(),
+                "text": field.css("::text").extract(),
+                "clean": field.css("::text").extract_first().strip(),
+            }
+            for field in row.css("td")
+        ]
